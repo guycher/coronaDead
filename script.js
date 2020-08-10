6c06ad36e4d8b2dddf5159c44928c1c519b278fa
doron <dgescheidt3@gmail.com>
2020-08-10 18:39:31 +0300

    working now!

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-$(function () {
-    $('#table').bootstrapTable({
-        data: mydata
-    });
-});
-
-
-var $table = $('#table');
-    var mydata = []
-    mydata = fetch('coronaDead1.json')
-        .then(results => results.json())
-        .then(console.log)
-
-
-/*
-
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "coronaDead1.csv",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
-});
-
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
-        }
-    }
-    alert(lines);
+function fillTable() {
+    fetch('coronaDead1.json')
+     .then(results => results.json())
+     .then(mydata => {
+        //console.log(mydata)
+        $('#table').bootstrapTable({
+            data: mydata
+        })
+     })
 }
-*/
+
+async function testFetch() {
+   const result = await fetch('coronaDead1.json')
+   const mydata = await result.json()
+   $('#table').bootstrapTable({
+        data: mydata
+   })
+}
+
+//fillTable()
+testFetch()
